16967dbcd7a05f062ef8111c54484b39ebad4dd5
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-08-20 09:08:25 +0530

    Time: 12 ms (61.06%), Space: 43.5 MB (83.75%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,34 +1,61 @@
 class Solution {
-    public String predictPartyVictory(String senate) {
-        Deque<Character> q = new ArrayDeque<>();
-        for(char ch: senate.toCharArray()) q.add(ch);
+    public String predictPartyVictory(String senate){
+        int n = senate.length();
+        Deque<Integer> rad = new ArrayDeque<>();
+        Deque<Integer> dir = new ArrayDeque<>();
         
-        while(q.size()>1){
-            char ch = q.remove();
-            if(ch=='R'){
-                int count = 0;
-                while(q.size()>0 && q.peek()!='D'){
-                    q.remove();
-                    count++;
-                }
-                if(q.size()==0) return "Radiant";
-                q.remove();
-                while(count-->0) q.addFirst('R');
-                q.addLast(ch);
+        for(int i=0;i<n;i++){
+            if(senate.charAt(i)=='R') rad.add(i);
+            else dir.add(i);
+        }
+      
+        while(rad.size()>0 && dir.size()>0){
+            if(rad.peek() < dir.peek()){
+                rad.add(n++);
+                rad.remove();
+                dir.remove();
             }
             else{
-                int count = 0;
-                while(q.size()>0 && q.peek()!='R'){
-                    q.remove();
-                    count++;
-                }
-                if(q.size()==0) return "Dire";
-                q.remove();
-                while(count-->0) q.addFirst('D');
-                q.addLast(ch);
+                dir.add(n++);
+                dir.remove();
+                rad.remove();
             }
         }
         
-        return q.peek() == 'R' ? "Radiant" : "Dire";
+        return rad.size()==0 ? "Dire" : "Radiant";
     }
+    
+    
+//     public String predictPartyVictory(String senate) {
+//         Deque<Character> q = new ArrayDeque<>();
+//         for(char ch: senate.toCharArray()) q.add(ch);
+        
+//         while(q.size()>1){
+//             char ch = q.remove();
+//             if(ch=='R'){
+//                 int count = 0;
+//                 while(q.size()>0 && q.peek()!='D'){
+//                     q.remove();
+//                     count++;
+//                 }
+//                 if(q.size()==0) return "Radiant";
+//                 q.remove();
+//                 while(count-->0) q.addFirst('R');
+//                 q.addLast(ch);
+//             }
+//             else{
+//                 int count = 0;
+//                 while(q.size()>0 && q.peek()!='R'){
+//                     q.remove();
+//                     count++;
+//                 }
+//                 if(q.size()==0) return "Dire";
+//                 q.remove();
+//                 while(count-->0) q.addFirst('D');
+//                 q.addLast(ch);
+//             }
+//         }
+        
+//         return q.peek() == 'R' ? "Radiant" : "Dire";
+//     }
 }
