154c32df03087f3ca2a7a18dd6cdf10d69108b6b
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-08-26 08:30:45 +0530

    Time: 8 ms (99.48%), Space: 43.8 MB (72.14%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,14 +1,24 @@
 class Solution {
-    public int findLongestChain(int[][] intervals) {
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
-        int maxMeet=0;
-        int lim=Integer.MIN_VALUE;
-        for(int i=0;i<intervals.length;i++){
-            if(intervals[i][0]>lim){
-                maxMeet++;
-                lim=intervals[i][1];
+    /* If we sort the array according to second element of pair then we just have to check for first element, Since first element is less than second.
+        1) Sort the array according to right element
+        2) For each pair just check if first element is greater than previous last element and increase the chain length by 1 
+        3) After increasing length also change  last element to current last elemet
+    
+    */
+    
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b) -> a[1]-b[1]);      //Sorting array according to right element
+        
+        int longestChain = 0;
+        int currChain = Integer.MIN_VALUE;
+        
+        for(int i=0; i<pairs.length; i++){
+            if(pairs[i][0]>currChain){
+                longestChain++;
+                currChain = pairs[i][1];
             }
         }
-        return maxMeet;
+        
+        return longestChain;
     }
 }
