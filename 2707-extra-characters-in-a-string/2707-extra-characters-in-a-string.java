c42fa762015e16427495e21d762558514621f44b
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-09-02 12:18:51 +0530

    Time: 34 ms (84.57%), Space: 44.6 MB (15.71%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int minExtraChar(String s, String[] dict) {
+    public int minExtraChar(String s, String[] dictionary) {
         int n = s.length();
-        int m = dict.length;
         int[] dp = new int[n+1];
         
         for(int i=n-1;i>=0;i--){
             int maxLen = 0;
-            for(String word: dict){
+            for(String word: dictionary){
                 int wordLen = word.length();
-                    
+                 
+               // check if the remaining substring is less than or equal to dictonary word
                 if(n-i >= wordLen){
+                    //check if the remaining substring is equal to dictionary word
+                    
                     if(s.substring(i,i+wordLen).equals(word)){
                         maxLen = Math.max(maxLen,wordLen+dp[i+wordLen]);
                     }
