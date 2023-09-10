716d1a512d2b186099cc5ea77d5fb0bdec181127
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-09-10 09:16:06 +0530

    Time: 3 ms (91.57%), Space: 44 MB (26.03%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,27 +1,39 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
         int n = arr.length;
-        PriorityQueue<Integer>[] bucket = new PriorityQueue[32];
         
-        for(int num: arr){
-            int bits = Integer.bitCount(num);
-            if(bucket[bits]==null){
-                bucket[bits] = new PriorityQueue<>();    
-            }
-            bucket[bits].add(num);
-        }
+        for(int i=0; i<n; i++) arr[i] += Integer.bitCount(arr[i])*100000;
         
-        int[] res = new int[n];
-        int idx = 0;
-        for(int i=0;i<32;i++){
-            if(bucket[i] != null){
-                while(bucket[i].size()>0){
-                    res[idx++] = bucket[i].remove();
-                }
-            }
-        }
+        Arrays.sort(arr);
         
-        return res;
+        for(int i=0; i<n; i++) arr[i] = arr[i]%100000;
         
+        return arr;
     }
+    
+//     public int[] sortByBits(int[] arr) {
+//         int n = arr.length;
+//         PriorityQueue<Integer>[] bucket = new PriorityQueue[32];
+        
+//         for(int num: arr){
+//             int bits = Integer.bitCount(num);
+//             if(bucket[bits]==null){
+//                 bucket[bits] = new PriorityQueue<>();    
+//             }
+//             bucket[bits].add(num);
+//         }
+        
+//         int[] res = new int[n];
+//         int idx = 0;
+//         for(int i=0;i<32;i++){
+//             if(bucket[i] != null){
+//                 while(bucket[i].size()>0){
+//                     res[idx++] = bucket[i].remove();
+//                 }
+//             }
+//         }
+        
+//         return res;
+        
+//     }
 }
