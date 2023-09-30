904d54c28b74c458ea8940139f8d00ac32738f27
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-09-30 16:11:05 +0530

    Time: 24 ms (63.06%), Space: 63.1 MB (52.15%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div456-132-pattern.java
@@ -1,25 +1,50 @@
 class Solution {
+    //Initialize kth ele to integer.min_value
+    //stk will contain the jth element so if ith element is greater than jth then 
+    //make jth ele to kth and ith to jth
+    //if curr element is less than kthelement means we found the ans
+    
     public boolean find132pattern(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
+        int kthEle =  Integer.MIN_VALUE;
         
-        int[] leftSmallest = new int[n];
-
-        int smallest = nums[0];
-        for(int i=1;i<n;i++){
-            leftSmallest[i] = smallest;
-            smallest = Math.min(smallest,nums[i]);
-        }
-
-        Stack<Integer> stk = new Stack<>();
-        stk.push(nums[n-1]);
-        for(int i=n-2;i>0;i--){
-            if(nums[i]<=leftSmallest[i]) continue;
-            while(stk.size()>0 && stk.peek()<=leftSmallest[i]) stk.pop();
+        Stack<Integer> stk = new Stack<>(); //to store jth element
+        
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<kthEle) return true;
             
-            if(stk.size()>0 && stk.peek()<nums[i]) return true;
+            while(stk.size()>0 && stk.peek()<nums[i]){
+                kthEle = stk.pop();
+            }
+            
             stk.push(nums[i]);
         }
         
         return false;
     }
+    
+    //using minimum element from left and stack
+//     public boolean find132pattern(int[] nums) {
+//         int n = nums.length;
+        
+//         int[] leftSmallest = new int[n];
+
+//         int smallest = nums[0];
+//         for(int i=1;i<n;i++){
+//             leftSmallest[i] = smallest;
+//             smallest = Math.min(smallest,nums[i]);
+//         }
+
+//         Stack<Integer> stk = new Stack<>();
+//         stk.push(nums[n-1]);
+//         for(int i=n-2;i>0;i--){
+//             if(nums[i]<=leftSmallest[i]) continue;
+//             while(stk.size()>0 && stk.peek()<=leftSmallest[i]) stk.pop();
+            
+//             if(stk.size()>0 && stk.peek()<nums[i]) return true;
+//             stk.push(nums[i]);
+//         }
+        
+//         return false;
+//     }
 }
