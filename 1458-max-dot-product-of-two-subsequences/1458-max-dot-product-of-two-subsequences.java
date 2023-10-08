a702b439e4b0d99eeceede4532932e6fda4bf676
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-10-08 11:06:34 +0530

    Time: 8 ms (98.61%), Space: 43 MB (80.56%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,28 +1,24 @@
 class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int[][] memo = new int[nums1.length+1][nums2.length+1];
-        for(int i=0;i<memo.length;i++){
-            Arrays.fill(memo[i],Integer.MIN_VALUE);
+        int m = nums1.length, n = nums2.length;
+        
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i=0;i<=m;i++){
+            Arrays.fill(dp[i],Integer.MIN_VALUE);
         }
         
-        return helper(0,0,nums1,nums2,memo);
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int x = (nums1[i]*nums2[j]) + Math.max(dp[i+1][j+1], 0);
+                int y = dp[i+1][j];
+                int z = dp[i][j+1];
+                
+                dp[i][j] = Math.max(x,Math.max(y,z));
+            }
+        }
+        
+        return dp[0][0];
     }
     
-    private int helper(int i, int j, int[] nums1, int[] nums2, int[][] memo){
-        if(i==nums1.length-1 && j==nums2.length-1) return memo[i][j] = nums1[i]*nums2[j];
-        
-        if(i==nums1.length || j== nums2.length) return Integer.MIN_VALUE;
-    
-        if(memo[i][j] != Integer.MIN_VALUE) return memo[i][j];
-        
-        int x = helper(i+1,j+1,nums1,nums2,memo);
-        x = (nums1[i]*nums2[j]) + Math.max(x,0);
-        int y = helper(i+1,j,nums1,nums2,memo);
-        
-        int z = helper(i,j+1,nums1,nums2,memo);
-        
-        return memo[i][j] = Math.max(x,Math.max(y,z));
-        
-        
-    }
 }
