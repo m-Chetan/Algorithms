ece1beb0c2b653aff6a5bc477b52391472e9e2cd
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-10-23 10:07:07 +0530

    Time: 1 ms (76.31%), Space: 39.4 MB (58.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        double res = Math.log(n)/Math.log(4);
-        return res == (int)res;
+    public boolean isPowerOfFour(int num) {
+        //num & (num-1) means number is power of 2 and  0x55555555 this represents number with set bit '1' at odd positions
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0x55555555) == num);
     }
 }
