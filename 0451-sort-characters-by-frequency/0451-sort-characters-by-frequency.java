98550756b7ec4e970a41e020cb1367384f1d22e4
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-12-03 11:17:24 +0530

    Time: 6 ms (89.72%), Space: 45.8 MB (5.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,10 +1,14 @@
 class Solution {
     public String frequencySort(String s) {
         int n = s.length();
+        
+        //There are total 256 characters possible lower + upper + digits + special symbol 
+        //Count frequency of each character
         int[] freq = new int[256];
+        for(char ch:s.toCharArray()) freq[ch]++;
         
-        for(char c:s.toCharArray()) freq[c]++;
-        
+        //create an array of array list that stores frequecy and add characters in list which have that frequency
+        //E.g  aabbc  2->[a,b] and 1->[c] 
         ArrayList<Integer>[] freqCount = new ArrayList[n+1];
         for(int i=0;i<=n;i++) freqCount[i] = new ArrayList<>();
         
@@ -13,9 +17,10 @@ class Solution {
                 freqCount[freq[i]].add(i);
             }
         } 
-   
         
         StringBuilder res = new StringBuilder();
+        
+        //Traverse from end since its the highest frequecy and we want to sort in decreasing
         
         for(int i=n;i>=0;i--){
             if(freqCount[i].size() != 0){
