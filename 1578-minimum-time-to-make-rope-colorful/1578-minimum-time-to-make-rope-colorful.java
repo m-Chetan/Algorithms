99de95ada78e74b95fca4e984a7f7ff66d806146
m-Chetan <61611588+m-Chetan@users.noreply.github.com>
2023-12-27 14:15:01 +0530

    Time: 9 ms (61.18%), Space: 60.8 MB (14.14%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,24 +1,50 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int n = neededTime.length;
+    
+        int sum = 0;
+        for(int time: neededTime) sum += time;
         
         int res = 0;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int i=0;
         
         while(i<n){
+            int max = neededTime[i];
             while(i<n-1 && colors.charAt(i) == colors.charAt(i+1)){
-                pq.add(neededTime[i]);
+                max = Math.max(max,neededTime[i]);
                 i++;
             }
-            pq.add(neededTime[i]);
+            max = Math.max(max,neededTime[i]);
             
-            while(pq.size()>1) res += pq.remove();
-            pq.remove();
+            res += max;
             i++;
         }
         
-        return res;
+        return sum-res;
     }
+    
+    
+//     public int minCost(String colors, int[] neededTime) {
+//         int n = neededTime.length;
+        
+//         int res = 0;
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         int i=0;
+        
+//         while(i<n){
+//             while(i<n-1 && colors.charAt(i) == colors.charAt(i+1)){
+//                 pq.add(neededTime[i]);
+//                 i++;
+//             }
+//             pq.add(neededTime[i]);
+            
+//             while(pq.size()>1) res += pq.remove();
+//             pq.remove();
+//             i++;
+//         }
+        
+//         return res;
+//     }
 }
